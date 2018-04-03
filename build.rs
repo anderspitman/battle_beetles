a617207c9e5042374fe0f92f6ef687308c802c8f
Anders Pitman <tapitman11@gmail.com>
2018-04-03 01:37:50 -0700

    Integrate js and rust builds into build.rs

--- build.rs
+++ build.rs
@@ -1,11 +1,49 @@
 extern crate protoc_rust;
 
+use std::process::Command;
+
 fn main() {
+    
+    // generate rust protobuf files
     protoc_rust::run(protoc_rust::Args {
         out_dir: "src/gen",
         input: &["protos/messages.proto"],
         includes: &["protos"],
     }).expect("protoc");
 
-    println!("cargo:rerun-if-changed=protos");
+    // generate js protobuf files
+    let status = Command::new("protoc").args(&[
+        "--proto_path=protos",
+        "--js_out=import_style=commonjs,binary:ui/gen",
+        "protos/messages.proto"]).status();
+    match status {
+        Ok(s) => {
+            if !s.success() {
+                println!("protoc failed: {}", s);
+            }
+        },
+        Err(e) => {
+            println!("protoc command failed: {}", e);
+        }
+    };
+
+    // bundle js
+    let status = Command::new("npm").args(&[
+        "run", "--prefix", "ui", "build",
+    ]).status();
+    match status {
+        Ok(s) => {
+            if !s.success() {
+                println!("npm failed: {}", s);
+            }
+        },
+        Err(e) => {
+            println!("npm command failed: {}", e);
+        }
+    };
+
+    println!("cargo:rerun-if-changed=protos/messages.proto");
+    println!("cargo:rerun-if-changed=ui/index.html");
+    println!("cargo:rerun-if-changed=ui/index.js");
+    println!("cargo:rerun-if-changed=ui/message_service.js");
 }
